a5d13943d49708ac5a5276e5a31618b457cd69df
ArturoEsp <55172579+ArturoEsp@users.noreply.github.com>
2020-07-20 18:28:07 -0500

    push aux
    oliwis

---
+++ View/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Experto Vocacional</title>
+    <link rel="stylesheet" href="..\css\style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet">
+</head>
+
+<body class="bg-index">
+
+    <div class="containter-title-body">
+        <img src="..\assets\images\logo.png" >
+    </div>
+
+    <div class="container-body">
+
+        <div class="text-body">
+            <span>¿Quieres vivir lo que te apasiona?</span>
+            <h2>Encuentra Ahora<br>Tu Carrera</h2>
+
+            <a class="btn-body" href="registro-usuario.php">Registrate</a>
+
+            <a class="btn-body-2" style="margin-left: 25px;" href="login.html">Iniciar sesión</a>
+
+        </div>
+
+        <div class="image-body">
+            <img src="..\assets\images\image-1.jpg" width="620px">
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ View/registro-usuario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Registrar Usuario</title>
+</head>
+<body>
+    <?php
+include 'menu-header.html';
+?>
+    <div class="container-form">
+        <h2>Registrar nuevo usuario</h2>
+        <span>Ingresa tus datos correspondientes</span>
+        <br>
+        <div class="form-principal">
+            <form class="frm" action="">
+                <div class="input-field">
+                    <label>Nombre</label>
+                    <input type="text" value="" maxlength="30" class="input-text">
+                </div>
+
+                <div class="input-field">
+                    <label>Usuario</label>
+                    <input type="text" value="" maxlength="15" class="input-text">
+                </div>
+
+                <div class="input-field">
+                    <label>Contraseña</label>
+                    <input type="password" value="" maxlength="15" class="input-text">
+                </div>
+
+                <input type="button" value="Registrarse" class="btn-form">
+                
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
