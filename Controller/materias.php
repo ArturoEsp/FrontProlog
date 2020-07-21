c6e177f2a7ad32e95b6f740d92d2974968270044
Antonio <67932072+AntonioBellito@users.noreply.github.com>
2020-07-21 18:02:57 -0500

    update materias
    materias php

--- Controller/materias.php
+++ Controller/materias.php
@@ -11,7 +11,7 @@ if (isset($_POST['Nombre'])){
     $json->nombre = $_POST["Nombre"];
 
     $curl = new stdClass();
-    $curl->URL = "http://192.168.99.100/ProyectoProlog/public/api/validation";
+    $curl->URL = "http://192.168.99.100/ProyectoProlog/public/api/carreras";
     $curl->VERBO = "GET";
     $curl->DATA = json_encode($json);
 
@@ -20,5 +20,21 @@ if (isset($_POST['Nombre'])){
     $resultado = $data->ApiRest($curl);
 
     $jsonresultado = json_decode($resultado->body);
+}
 
+if (isset($_GET['option']=='delete')){
+    
+    $json = new stdClass();
+    $json->nombre = $_POST["IdMateria"];
+
+    $curl = new stdClass();
+    $curl->URL = "http://192.168.99.100/ProyectoProlog/public/api/carreras";
+    $curl->VERBO = "DELETE";
+    $curl->DATA = json_encode($json);
+
+    $data = new cURLRequest();
+
+    $resultado = $data->ApiRest($curl);
+
+    $jsonresultado = json_decode($resultado->body);
 }
