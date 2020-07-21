744681b9b714e1281185902fe25d406fbdd42fec
ArturoEsp <55172579+ArturoEsp@users.noreply.github.com>
2020-07-21 17:41:00 -0500

    cambios random
    sdasdadad

--- Controller/validation.php
+++ Controller/validation.php
@@ -1,6 +1,14 @@
 <?
 include_once 'cURLRequest.php';
 
+$userSession = new UserSession();
+
+
+if (isset($_SESSION['user'])){
+    
+}
+
+
 if (isset($_POST['Usuario']) && isset($_POST['Contrasena'])) {
 
     $json = new stdClass();
@@ -20,8 +28,33 @@ if (isset($_POST['Usuario']) && isset($_POST['Contrasena'])) {
 
 
     if($jsonresultado->mensaje == 1){
-        header('Location: ../View/index.php');
-    }else{
+        
+        $json = new stdClass();
+        $json->usuario = $_POST["Usuario"];
+        
+        $curl = new stdClass();
+        $curl->URL = "http://192.168.99.100/ProyectoProlog/public/api/usuario";
+        $curl->VERBO = "GET";
+        $curl->DATA = json_encode($json);
+    
+        $data = new cURLRequest();
+    
+        $resultado = $data->ApiRest($curl);
+        
+        $jsonresultado = json_decode($resultado->body);
+        
+        if($jsonresultado->Rol == 1)
+        {
+            $userSession->setCurrentUser("1");
+        }
+        else
+        {
+            $userSession->setCurrentUser("0");
+        }
+
+    }
+    
+    else{
         header('Location: ../View/login.html');
     } 
     
