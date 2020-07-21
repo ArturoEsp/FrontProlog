5e7c111117b2094981b5abdcb601939ec91fa5e1
Antonio <67932072+AntonioBellito@users.noreply.github.com>
2020-07-21 17:35:18 -0500

    Create materias en Controller
    Creacion de el php para materias

---
+++ Controller/materias.php
@@ -0,0 +1,24 @@
+<?
+include_once 'cURLRequest.php';
+
+if (isset($_SESSION['user'])){
+
+}
+
+if (isset($_POST['Usuario']) && isset($_POST['Contrasena']){
+    
+    $json = new stdClass();
+    $json->nombre = $_POST["Nombre"];
+
+    $curl = new stdClass();
+    $curl->URL = "http://192.168.99.100/ProyectoProlog/public/api/validation";
+    $curl->VERBO = "GET";
+    $curl->DATA = json_encode($json);
+
+    $data = new cURLRequest();
+
+    $resultado = $data->ApiRest($curl);
+
+    $jsonresultado = json_decode($resultado->body);
+
+}
