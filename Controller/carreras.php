2903ec3c9296b583d7283de72d9ce17690e04f6f
Victor Andaya <48934786+VictorAndaya@users.noreply.github.com>
2020-07-21 18:54:46 -0500

    Update carreras.php

--- Controller/carreras.php
+++ Controller/carreras.php
@@ -1,15 +1,16 @@
 <?
 include_once 'cURLRequest.php';
 
-if (isset($_POST['Nombre'])){
+if (isset($_POST["Nombre"])){
+
     $json = new stdClass();
     $json->Nombre = $_POST["Nombre"];
     
     $curl = new stdClass();
-    $curl->URL = "http://192.168.99.100/proyecto/public/api/carrera";
+    $curl->URL = "http://localhost/ProyectoProlog/public/api/carrera";
     $curl->VERBO = "POST";
     $curl->DATA = json_encode($json);
-
+    
     $data = new cURLRequest();
 
     $resultado = $data->ApiRest($curl);
@@ -22,7 +23,7 @@ if (isset($_GET['option']=='delete')){
     $json->IdCarrera = $_GET["IdCarrera"];
     
     $curl = new stdClass();
-    $curl->URL = "http://192.168.99.100/proyecto/public/api/carreradelete";
+    $curl->URL = "http://localhost/ProyectoProlog/public/api/carreradelete";
     $curl->VERBO = "DELETE";
     $curl->DATA = json_encode($json);
 
