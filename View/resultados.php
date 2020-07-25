b3008d5d27be5f7545b3c486749bdaba6171c9e4
Antonio <67932072+AntonioBellito@users.noreply.github.com>
2020-07-24 21:08:01 -0500

    Se agregaron las vistas para los resultados
    Holi

---
+++ View/resultados.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/w3.css">
+    <title>Resultados</title>
+</head>
+
+<body>
+    <?php
+    include 'menu-header.html';
+    ?>
+    <div class="w3-container w3-padding-16">
+        <h2 class="w3-text-blue w3-center"> RESULTADOS </h2>
+        <div class="w3-content w3-row-padding">
+            <div class="w3-third">
+                <div class="w3-card-4 w3-padding w3-center">
+                <img src="..\assets\images\tercer.png"  width="60%">
+                    <h2>Segundo resultado</h2>
+                    <p>
+                        <br>
+                        <h4>IGE por que te gustan los culos</h4>
+                        <br>
+                    </p>
+                </div>
+
+            </div>
+
+            <!---->
+            <div class="w3-third">
+                <div class="w3-card-4 w3-padding w3-center">
+                <img src="..\assets\images\primer.png"  width="60%">
+                    <h2>Primer resultado</h2>
+                    <p>
+                    <br>
+                        <h4>La que tu quieras rey, por que eres pro <3</h4>
+                        <br>
+                    </p>
+                </div>
+
+            </div>
+
+            <div class="w3-third">
+                <div class="w3-card-4 w3-padding w3-center">
+                <img src="..\assets\images\segundo.png" width="60%">
+                    <h2>Tercer resultado</h2>
+                    <p>
+                    <br>
+                        <h4>Turismo por que las matematicas te dan ansiedad</h4>
+                        <br>
+                    </p>
+                </div>
+
+            </div>
+            <!---->
+            
+        </div>
+    </div>
+
+    
+
+
+
+</body>
+
+</html>
